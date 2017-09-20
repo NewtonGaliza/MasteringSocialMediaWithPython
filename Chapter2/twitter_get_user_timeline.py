cd263cfc43df6a962d40c195efd495fc6133788d
Newton Galiza <newtonjgaliza@gmail.com>
2017-09-20 18:43:22 -0300

    Create twitter_get_user_timeline.py

---
+++ Chapter2/twitter_get_user_timeline.py
@@ -0,0 +1,17 @@
+#chap02-3/twitter_get_user_timeline.py
+
+import sys
+import json
+from tweepy import Cursor
+from twitter_client import get_twitter_client
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    client = get_twitter_client()
+
+    fname = 'user_timeline_{}.jsonl'.format(user)
+
+with open(fname, 'w') as f:
+    for page in Cursor(client.user_timeline, screen_name=user, count=200).pages(16):
+        for status in page:
+            f.write(json.dumps(status._json)+'\n')
