420785c9c0cf6fa80bc1cdf2e0d64bc44526eaba
Newton Galiza <newtonjgaliza@gmail.com>
2017-09-20 18:41:58 -0300

    Create twitter_client.py

---
+++ Chapter2/twitter_client.py
@@ -0,0 +1,23 @@
+# Chap02/twitter_client.py
+import os
+import sys
+from tweepy import API
+from tweepy import OAuthHandler
+
+def get_twitter_auth():
+    try:
+        consumer_key = os.environ['TWITTER_CONSUMER_KEY']
+        consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
+        access_token = os.environ['TWITTER_ACCESS_TOKEN']
+        access_secret = os.environ['TWITTER_ACCESS_SECRET']
+    except KeyError:
+        sys.stderr.write("TWITTER_* environment variables not set\n")
+        sys.exit(1)    
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    return auth
+
+def get_twitter_client():
+    auth = get_twitter_auth()
+    client = API(auth)
+    return client
